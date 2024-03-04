493b464f756e79346b3e091145da8df2eb09d3c0
mdsajalali <mdsajalali2020@gmail.com>
2024-03-04 18:44:32 +0600

    products image not found issues fixed

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -25,12 +25,12 @@ const Product: React.FC<ProductProps> = ({ product }) => {
   return (
     <div>
       <div className="border-x border-t">
-        <img src={product.img} alt="" />
+        <img src={product?.img} alt="" />
       </div>
       <div className="border px-4">
         <div>
-          <p className="font-semibold text-gray-500 mt-2">{product.name}</p>
-          <p className="my-1">{product.title}</p>
+          <p className="font-semibold text-gray-500 mt-2">{product?.name}</p>
+          <p className="my-1">{product?.title}</p>
         </div>
         <div className="flex gap-5 items-center justify-between">
           <div className="flex">
@@ -46,7 +46,7 @@ const Product: React.FC<ProductProps> = ({ product }) => {
           onClick={handleItem}
           className="flex items-center justify-between mb-5"
         >
-          <h1 className="font-bold text-[18px] mt-5">{product.price}</h1>
+          <h1 className="font-bold text-[18px] mt-5">{product?.price}</h1>
           <button className=" mt-5 px-3 py-2 rounded-sm bg-[#DC2626]  hover:bg-[#0989FF]   text-white transition-all font-semibold flex items-center gap-2">
             <FiShoppingCart size={20} /> Add To Cart
           </button>
